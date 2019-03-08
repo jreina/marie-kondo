1fb9600b08744bd702775942df9dd9308222e3d5
Johnny Reina <johnny.r.reina@gmail.com>
2019-03-07 22:40:24 -0600

    Adds cli

---
+++ bin/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const fs = require('fs');
+const tidyUp = require('../src/index');
+
+program
+  .version('0.0.1')
+  .usage('[options] <dir>')
+  .arguments('<dir>')
+  .option('-f, --fix', 'Tidy up')
+  .option(
+    '-p, --probability <n>',
+    'Probability that a given file will be discarded [0.5]',
+    0.5,
+    parseFloat
+  )
+  .action(dir => {
+    if (!fs.existsSync(dir)) throw `Path does not exist! ${dir}`;
+
+    tidyUp(dir, program.probability, !program.fix);
+  });
+
+program.parse(process.argv);
